a597f1955657088bf980a2eb718d7b25664fd7ff
Adam Mausenbaum <adam@admm.io>
2016-11-10 14:49:29 +0200

    Read mocks for each request

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
-const mocks = require('./mocks.json');
 
 app.get('/api/forms/:id', function (req, res) {
-    res.send(mocks[0]);
+    fs.readFile('./mocks.json', (err, data) => {
+        const mocks = JSON.parse(data);
+        res.send(mocks[0]);
+    })
 });
 
 app.listen(3000, function () {

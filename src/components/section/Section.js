62d6a712e966187288116f516e9a4b94b0a819de
Adam Mausenbaum <adam@admm.io>
2016-11-11 09:37:59 +0200

    Add propTypes

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import Question from '../question/Question';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 
 const Section = (props) =>  (
 	<Card initiallyExpanded={true}>
-	<CardHeader
-      title={props.title}
-      subtitle={props.description}
-      actAsExpander={true}
-      showExpandableButton={true}
-    />
-		<CardText expandable={true}>
-			{props.questions.map(question => {
-				return <Question {...question} />
-			})}
-		</CardText>
+	    <CardHeader
+          title={props.title}
+          subtitle={props.description}
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardText expandable={true}>
+            {props.questions.map(question => {
+                return <Question {...question} />
+            })}
+        </CardText>
 	</Card>
 );
+
+Section.propTypes = {
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string,
+	questions: PropTypes.array.isRequired
+};
 
 export default Section;
